c93b0811fda51fbf04880865873a6fa8bfc528d5
James <james@smoothbook.co>
2017-07-06 15:55:40 +0700

    Styling updates

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,18 +2,34 @@ import React from 'react';
 
 export default class Header extends React.Component {
 
+    doSearch(ev){
+
+        console.log(ev);
+        ev.preventDefault();
+
+        alert('hello');
+
+        return false;
+    }
+
     render() {
         return (
 
             <header>
 
                 <div className="header-block-left">
-                    <i className="fa fa-bar-chart" aria-hidden="true"></i>
+                    &nbsp;
                 </div>
 
                 <div className="header-block-right">
 
                     <span className="link-name">Link Blockchain Explorer</span>
+
+                    <form className="item-search" onSubmit={this.doSearch}>
+
+                        <input type="text" className="form-control" placeholder="Search account / txn / block" />
+
+                    </form>
 
                 </div>
 
--- src/layout/Navleft.jsx
+++ src/layout/Navleft.jsx
@@ -20,12 +20,12 @@ export default class Navleft extends React.Component{
                         </li>
                         <li>
                             <Link to="/account">
-                                <i className="fa fa-balance-scale" aria-hidden="true"></i>
+                                <i className="fa fa-search" aria-hidden="true"></i>
                             </Link>
                         </li>
                         <li>
                             <Link to="/">
-                                <i className="fa fa-arrows" aria-hidden="true"></i>
+                                <i className="fa fa-cog" aria-hidden="true"></i>
                             </Link>
                         </li>
                     </ul>
