b93ee96b0a8d82ded87b3c9e4b341e3c928f14f5
James <james@smoothbook.co>
2017-07-13 16:46:37 +0700

    Create contract ok

---
+++ src/lib/LinkContract.js
@@ -0,0 +1,52 @@
+
+
+export default class LinkContract{
+
+    constructor(web3){
+
+        this._web3 = web3;
+
+    }
+
+    compileSolidity(soliditySource){
+
+        // Compile contract from *flattened* solidity source.
+        return this._web3.compile.solidity(soliditySource);
+
+    }
+
+    // Attempts to add a new contract to the blockchain, returns a handle to the contract
+    // instance on success, error message on failure.
+    createNew(ABI, compiledCode, constructorArgs = null, paymentAccount, gas, callBack){
+
+        // Create a 'template' of the contract from the ABI.
+        const contractTemplate = this._web3.eth.contract(ABI);
+
+        // Now attempt to add the contract to the block chain.
+        // - Arguments are passed to the contract constructor via constructorArgs
+        // - Adding a contract requires payment for the transaction: an account and a gas limit.
+
+        const contractOptions = {
+            from : paymentAccount,
+            gas : gas,
+            data : compiledCode
+        };
+
+       return contractTemplate.new(constructorArgs, contractOptions,
+           (err, contractDetails)=>{
+
+                if(err) throw new Error(err.message);
+
+                const newContract = {
+                    address : contractDetails.address,
+                    transaction : contractDetails.transactionHash
+                };
+
+                callBack(newContract);
+
+            }
+       );
+
+    }
+
+}
--- src/pages/Demo.jsx
+++ src/pages/Demo.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 
-import {Bar, defaults as chartDefaults} from 'react-chartjs-2';
+import LinkContract from '../lib/LinkContract.js';
 
 // Ethereum Explorer home page - displays system stats
 
@@ -13,21 +13,43 @@ export default class Demo extends React.Component {
 
         const web3 = this.props.linkClient.getWeb3Instance();
 
-        console.log('Accounts: ');
-        console.log(web3.eth.accounts[0]);
+        const contractData = {
+            ABI: [{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"}],
+            byteCode : '0x6060604052341561000f57600080fd5b5b336000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff1602179055505b5b61010c806100616000396000f30060606040526000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff16806341c0e1b514603d575b600080fd5b3415604757600080fd5b604d604f565b005b6000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff16141560dd576000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16ff5b5b5600a165627a7a72305820bc48462adefdde92681ae81f5209019cb834b97b474396c1865471eeb9999e980029'
+        };
 
         try{
 
-            web3.personal.unlockAccount(web3.eth.accounts[0], 'mistr4ls');
+            web3.personal.unlockAccount(web3.eth.accounts[0], 'password'); // Demo account, you still need a private key to unlock.
 
-        }catch(err){
+            const contract = new LinkContract(web3);
 
-            console.error(err.message);
+            const contractHandle = contract.createNew(contractData.ABI, contractData.byteCode, 'Hi there', web3.eth.accounts[0], 300000,
+                (result)=>{
+
+                    if(!result.address){
+                        return;
+                    }
+
+                    console.log('success');
+                    console.log(result);
+
+                }
+            )
+        }
+
+
+
+        catch(err){
+
+            alert(err.message);
 
         }
 
-        var solContract = web3.eth.contract([{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"}]);
-        var browser_untitled_sol_mortal = solContract.new(
+
+
+        /*var solContract = web3.eth.contract([{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"}]);
+        var greeter = solContract.new(
             {
                 from: web3.eth.accounts[0],
                 data: '0x6060604052341561000f57600080fd5b5b336000806101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff1602179055505b5b61010c806100616000396000f30060606040526000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff16806341c0e1b514603d575b600080fd5b3415604757600080fd5b604d604f565b005b6000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffffffffff16141560dd576000809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16ff5b5b5600a165627a7a723058201e9f42859114ba90cb3fe34b31a02000c128850b9cad5567207dbc8a666916590029',
@@ -43,7 +65,17 @@ export default class Demo extends React.Component {
                     console.log('Contract mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
                 }
 
-            })
+                try{
+
+                    console.log('Code: ' + web3.eth.getCode(contract.address));
+
+                }catch(err){
+
+                    console.log(err.message);
+
+                }
+
+            })*/
     }
 
 
