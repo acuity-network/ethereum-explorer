df1397f232894462b2ff4c1717c74a8b03869690
James <james@smoothbook.co>
2017-09-05 18:51:17 +0700

    Display block ok

--- src/pages/Block.jsx
+++ src/pages/Block.jsx
@@ -30,6 +30,8 @@ export default class Block extends React.Component{
             const block = this._link.getBlock(this.state.blockID);
             this.setState({ block : block });
 
+            console.log(block);
+
         }else{
 
             console.log('Getting last blocks');
@@ -42,7 +44,81 @@ export default class Block extends React.Component{
 
         if(this.state.block){
 
-            return <div className="alert alert-info"> This is the block </div>;
+            return(
+
+                <div className="block-display col-md-6 col-md-offset-3">
+
+                    <h2>Block Information</h2>
+
+                    <div className="table-responsive">
+
+                        <table className="table">
+
+                            <tbody>
+
+                            <tr>
+                                <td>
+                                    <label>number</label>
+                                </td>
+                                <td>
+                                    <span className="block-display-value">{this.state.block.number}</span>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <label>gas limit</label>
+                                </td>
+                                <td>
+                                    <span className="block-display-value">{this.state.block.gasLimit}</span>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <label>gas used</label>
+                                </td>
+                                <td>
+                                    <span className="block-display-value">{this.state.block.gasUsed}</span>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <label>difficulty</label>
+                                </td>
+                                <td>
+                                    <span className="block-display-value">{this.state.block.difficulty.toString(10)}</span>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <label>transactions</label>
+                                </td>
+                                <td>
+                                    <span className="block-display-value">{this.state.block.transactions.length}</span>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <label>uncles</label>
+                                </td>
+                                <td>
+                                    <span className="block-display-value">{this.state.block.uncles.length}</span>
+                                </td>
+                            </tr>
+
+                            </tbody>
+
+                        </table>
+
+                    </div>
+
+
+                </div>
+            )
 
         }else{
 
