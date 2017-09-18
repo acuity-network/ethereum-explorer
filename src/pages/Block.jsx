9eb7979d370321bcc49853ac20fb00e1b2130435
James <james@smoothbook.co>
2017-09-18 12:00:45 +0700

    Block list page

--- src/pages/Block.jsx
+++ src/pages/Block.jsx
@@ -1,49 +1,65 @@
-
 import React from 'react';
 
-export default class Block extends React.Component{
+export default class Block extends React.Component {
 
-    constructor(props){
+    constructor(props) {
 
         super(props);
 
         this._link = this.props.linkClient;
 
         this.state = {
-            blockID : null
+            blockID: null
         }
 
     }
 
-    componentDidMount(){
+    componentDidMount() {
 
         // If a second parameter is supplied to the URL, we're looking for a specific block.
         // If not, show the last ten blocks.
-        if (this.props.match.params && this.props.match.params.blockid){
+        if (this.props.match.params && this.props.match.params.blockid) {
 
-            this.state = {
-                blockID : this.props.match.params.blockid,
-                block : null
-            };
+            const blockID = this.props.match.params.blockid;
 
             // Search query has been defined as part of the url. Do search.
-            const block = this._link.getBlock(this.state.blockID);
-            this.setState({ block : block });
+            const block = this._link.getBlock(blockID);
+            this.setState(
+                {
+                    blockID : blockID,
+                    block: block,
+                    showMulti : false
+                }
+            );
 
-        }else{
+        } else {
 
+            // Get last ten blocks
             const blocks = this._link.getBlocks();
+
+            this.setState(
+                {
+                    blocks: blocks,
+                    showMulti : true
+                }
+            );
+
             console.log(blocks);
 
         }
 
     }
 
-    render(){
+    render() {
 
-        if(this.state.block){
+        if (this.state.showMulti) {
 
-            return(
+            return <div className="alert alert-info"> This is the list of blocks </div>;
+
+
+        } else {
+
+            return (
 
                 <div className="block-display col-md-6 col-md-offset-3">
 
@@ -87,7 +103,8 @@ export default class Block extends React.Component{
                                     <label>difficulty</label>
                                 </td>
                                 <td>
-                                    <span className="block-display-value">{this.state.block.difficulty.toString(10)}</span>
+                                    <span
+                                        className="block-display-value">{this.state.block.difficulty.toString(10)}</span>
                                 </td>
                             </tr>
 
@@ -118,10 +135,6 @@ export default class Block extends React.Component{
 
                 </div>
             )
-
-        }else{
-
-            return <div className="alert alert-info"> This is the list of blocks </div>;
 
         }
 
