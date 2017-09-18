0db474d9d9888a0716fe026b062a6792ecc183d0
James <james@smoothbook.co>
2017-09-18 11:31:38 +0700

    Small changes

--- config.js
+++ config.js
@@ -1,11 +1,15 @@
 
 const config = {
 
+    // Client url
     base_url : 'http://localhost:8080',
 
     // Geth / Parity must be run with rpc-cors set to '*' in order to allow the web3 api via AJAX.
     // Also reset the dapps port if you want to run webpack-dev-server
-    // parity --chain link.json --port 30313 --dapps-port 8000  --jsonrpc-port 8645 --geth --jsonrpc-cors '*'
+    // geth --config link.toml --datadir ~/.link-geth --rpc --rpccorsdomain '*' --rpcapi eth,net,web3,personal
+    // geth --datadir ~/.smooth-geth --nodiscover  --mine --etherbase 392aecc24aab21d043a307d795e9903949c15052
+
+    // Blockchain node
     node_uri : 'http://localhost:8645'
 
 };
