089d682c7916900802f23120cfa4f22de3213d30
James <james@smoothbook.co>
2017-09-20 12:05:32 +0700

    Transaction info

--- src/components/TransactionDisplay.jsx
+++ src/components/TransactionDisplay.jsx
@@ -9,10 +9,6 @@ export default class TransactionDisplay extends React.Component{
 
         console.log(this.props.transaction);
 
-        this.state = {
-            transaction : this.props.transaction
-        }
-
     }
 
     render(){
@@ -20,7 +16,25 @@ export default class TransactionDisplay extends React.Component{
         return(
             <div className="transaction-display col-md-6 col-md-offset-3">
 
-                <h2>Transaction Information</h2>
+                <h2 className="text-center">Transaction Information</h2>
+
+                <div className="transaction-from-to">
+
+                    <div className="text-center">
+
+                        <label className="transaction-from-to-label">from</label>
+                        <a href className="transaction-from-to-value">{this.props.transaction.from}</a>
+
+                    </div>
+
+                    <div className="text-center">
+
+                        <label className="transaction-from-to-label">to</label>
+                        <a href className="transaction-from-to-value">{this.props.transaction.from}</a>
+
+                    </div>
+
+                </div>
 
                 <div className="table-responsive">
 
