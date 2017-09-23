0cad9ab68ee61d134f5501f819c1f004c6838932
James <james@smoothbook.co>
2017-09-23 12:34:10 +0700

    Settings page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -36,12 +36,28 @@ export default class Settings extends React.Component {
 
             <h3>Connection settings</h3>
 
+            <div className="col-md-8 col-md-offset-2">
+
+                <p>
+                    The Ethereum Block Explorer can connect to an Ethereum blockchain via two methods:
+                </p>
+
+                <ul>
+                    <li>
+                        Blockchain node URI (e.g. http://[blockchain node]:[port]). Could be a blockchain node running on your
+                        localhost. The node will need to have CORS enabled for this to work.
+                    </li>
+                    <li>
+                        Metamask. See <a href="https://metamask.io/" target="_blank">https://metamask.io/</a>. Metamask is a clever
+                        Chrome and Firefox browser extension that manages blockchain connections and accounts for individual blockchains
+                        (including test and localhost nodes). You will need to install and enable Metamask for this option to work.
+                    </li>
+                </ul>
+
+
+            </div>
+
             <div className="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-
-                <div className="form-group">
-
-
-                </div>
 
             </div>
 
